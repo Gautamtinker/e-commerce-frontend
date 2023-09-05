b8f1dbc5b97c79a57746f97444fd8e9847aec08d
hima707ch <107808488+hima707ch@users.noreply.github.com>
2023-09-05 17:22:23 +0530

    Add files via upload

---
+++ Route/productRoute.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {addReview, createProduct , deleteProduct, deleteReview, getAllProduct, getAllReview, getProduct, updateProduct} from '../Controller/productController.js';
+import {isAuthUser, authorizeRoles} from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.route("/products").get( getAllProduct);
+
+router.route("/admin/products").get( getAllProduct);
+
+router
+.route("/admin/product/new")
+.post(isAuthUser, authorizeRoles("admin"), createProduct);
+
+router
+.route("/admin/product/:id")
+.put(isAuthUser, authorizeRoles("admin"), updateProduct)
+.delete(isAuthUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getProduct);
+
+router.route("/review").put( isAuthUser, addReview);
+
+router.route("/reviews").get(getAllReview).delete(isAuthUser, deleteReview)
+
+export default router;
